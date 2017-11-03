f2732462f9aed0d69962834bf6c5567548d6e23d
taher elsheikh <taher@redoakstrategic.com>
2017-11-02 23:17:38 -0400

    part one

---
+++ ravenous_1/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import BusinessList from './components/BusinessList/BusinessList.js';
+
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <h1>ravenous</h1>
+          <SearchBar />
+          <BusinessList />
+        </div>
+    );
+
+  }
+}
+
+export default App;
